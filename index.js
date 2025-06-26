e20c68b87d1eb78f17e3ed96549c9ade01ccc76d
Vkaisang41 <kaisang.vincent@student.moringaschool.com>
2025-06-26 10:31:27 +0300

    Remove main element and add victory header with custom message

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main id="main">
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element and assign to newHeader
+const newHeader = document.createElement('h1');
+
+// Set its id to 'victory'
+newHeader.id = 'victory';
+
+// Set its text content
+newHeader.textContent = "Vincent is the champion";
+
+// Append it to the body (so it's visible in the DOM)
+document.body.appendChild(newHeader);
